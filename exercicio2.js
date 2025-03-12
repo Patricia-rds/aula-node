2888b13162ea5508f89b360238b30ceb8973c040
Patrícia Santos <patricia.r.santos2001@gmail.com>
2025-03-12 17:10:45 -0300

    fiz exercicio2

---
+++ exercicio2.js
@@ -0,0 +1,35 @@
+/* 
+        ax ^ 2 + bx + c = 0
+
+Se o usuário informar o valor de A igual a zero, a equação não é do segundo
+    grau e o programa não deve fazer pedir os demais valores,
+    sendo encerrado;
+Se o delta calculado for negativo, a equação não possui raízes reais.
+    Informe ao usuário e encerre o programa;
+Se o delta calculado for igual a zero a equação possui apenas uma raiz
+    real; informe-a ao usuário;
+Se o delta for positivo, a equação possui duas raiz reais;
+    informe-as ao usuário; */
+
+let a = 5;
+let b = 10
+let c = 5
+let delta = b * b - (4 * a * c);
+
+if (a === 0)
+{
+    console.log("A equação não é de segundo grau. Encerrando o programa.");
+}
+else if (delta < 0)
+{
+    console.log("Não possui raízes reais.")
+}
+else if (delta === 0)
+{
+    console.log("A equação possui 1 raiz real.");
+}
+else
+{
+    console.log("A equação possui 2 raízes reais.")
+}
+
