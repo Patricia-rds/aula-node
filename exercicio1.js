00191e07f95f25eaff0708cb8ae1641b3e1ae90a
Patricia-rds <patricia.r.santos2001@gmail.com>
2025-03-11 21:48:53 -0300

    atualização

---
+++ exercicio1.js
@@ -0,0 +1,18 @@
+let entrada = "t";
+
+if ("v" == entrada)
+{
+    console.log("Boa Tarde!");
+}
+else if ("m" == entrada)
+{
+    console.log("Bom Dia!");
+}
+else if ("n" == entrada)
+{
+    console.log("Boa Noite!");
+}
+else
+{
+    console.log("Valor Inválido!")
+}
---
+++ funcoes.js
@@ -0,0 +1,14 @@
+// Entrada -> Processamento -> Saída
+
+function soma(n1, n2)
+{
+    let resultado = n1 + n2;
+    return resultado;
+}
+
+let res = soma(3,5);
+
+console.log(res);
+
+
+
